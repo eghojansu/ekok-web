fc65483fcf5211e2198a4e9df77443c1a195d03c
Eko Kurniawan <setiawanegho@gmail.com>
2021-01-15 07:38:16 +0700

    Extensible can be called case-insensitive

--- src/ExtensibleTrait.php
+++ src/ExtensibleTrait.php
@@ -9,13 +9,11 @@ trait ExtensibleTrait
 
     public function __call($name, $arguments)
     {
-        $extensions = $this->extensions[$name] ?? null;
-
-        if (!$extensions) {
+        if (!$this->checkExtension($name, $extension)) {
             throw new \BadMethodCallException("Extension method not exists: {$name}.");
         }
 
-        list($call, $wantThis) = $extensions;
+        list($call, $wantThis) = $this->extensions[$extension];
 
         return $wantThis ? $call($this, ...$arguments) : $call(...$arguments);
     }
@@ -23,6 +21,20 @@ trait ExtensibleTrait
     public function getExtensions(): array
     {
         return $this->extensions;
+    }
+
+    public function checkExtension(string $extension, string &$fixed = null): bool
+    {
+        if (isset($this->extensions[$extension])) {
+            $fixed = $extension;
+
+            return true;
+        }
+
+        $matches = preg_grep('/^' . preg_quote($extension, '/') . '$/i', array_keys($this->extensions));
+        $fixed = end($matches) ?: null;
+
+        return $fixed && isset($this->extensions[$fixed]);
     }
 
     public function extend(string $name, callable $function, bool $wantThis = false)
